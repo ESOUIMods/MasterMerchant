43451fd2d37736cdad0b0154602573255883cef1
Sharlikran <danielhmpdx16@gmail.com>
2020-11-06 18:08:19 -0800

    Version: 2.5.1

--- MasterMerchant_Namespace_Init.lua
+++ MasterMerchant_Namespace_Init.lua
@@ -26,7 +26,7 @@ MMScrollList.SORT_KEYS = {
 
 MasterMerchant = { }
 MasterMerchant.name = 'MasterMerchant'
-MasterMerchant.version = '2.5.0'
+MasterMerchant.version = '2.5.1'
 MasterMerchant.locale = 'en'
 MasterMerchant.viewMode = 'self'
 MasterMerchant.isScanning = false
