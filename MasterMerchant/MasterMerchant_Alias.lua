dac43f662aef167c9e133426ca633df0a81ce2b7
Sharlikran <danielhmpdx16@gmail.com>
2021-05-13 11:57:21 -0700

    Create MasterMerchant_Alias.lua

---
+++ MasterMerchant/MasterMerchant_Alias.lua
@@ -0,0 +1,19 @@
+local internal = _G["LibGuildStore_Internal"]
+
+-- A simple utility function to return which set of settings are active,
+-- based on the allSettingsAccount option setting.
+function MasterMerchant:ActiveSettings()
+  return MasterMerchant.systemSavedVariables
+end
+
+-- alias for previous function
+function MasterMerchant.makeIndexFromLink(itemLink)
+  return internal:MakeIndexFromLink(itemLink)
+end
+
+function MasterMerchant:TimeCheck()
+  --[[
+  this does nothing because LibPrice has no idea what it going on
+  don't mess with it or I'll make it local
+  ]]--
+end
