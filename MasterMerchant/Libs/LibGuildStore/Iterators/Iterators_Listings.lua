e964072a9840359d63ec9b67d8fb455c408d238f
Sharlikran <danielhmpdx16@gmail.com>
2021-09-18 10:04:23 -0700

    Added a few more strings

--- MasterMerchant/Libs/LibGuildStore/Iterators/Iterators_Listings.lua
+++ MasterMerchant/Libs/LibGuildStore/Iterators/Iterators_Listings.lua
@@ -383,9 +383,7 @@ function internal:IndexListingsData()
   local postfunc = function(extraData)
     internal:DatabaseBusy(false)
     if LibGuildStore_SavedVariables["showIndexingSummary"] then
-      internal:dm("Info",
-        string.format(GetString(GS_INDEXING_SUMMARY), GetTimeStamp() - extraData.start, extraData.indexCount,
-          extraData.wordsIndexCount))
+      internal:dm("Info", string.format(GetString(GS_INDEXING_SUMMARY), GetTimeStamp() - extraData.start, extraData.indexCount, extraData.wordsIndexCount))
     end
   end
 
