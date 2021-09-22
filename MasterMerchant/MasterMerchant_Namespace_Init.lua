69987594cdbd8911f8790d62858ad81741f3e095
Sharlikran <danielhmpdx16@gmail.com>
2021-09-21 19:05:00 -0700

    Version: 3.6.76
    Already fixed the libHistoireScanByTimestamp

--- MasterMerchant/MasterMerchant_Namespace_Init.lua
+++ MasterMerchant/MasterMerchant_Namespace_Init.lua
@@ -26,7 +26,7 @@ MMScrollList.SORT_KEYS = {
 
 MasterMerchant = { }
 MasterMerchant.name = 'MasterMerchant'
-MasterMerchant.version = '3.6.75'
+MasterMerchant.version = '3.6.76'
 
 -------------------------------------------------
 ----- early helper                          -----
