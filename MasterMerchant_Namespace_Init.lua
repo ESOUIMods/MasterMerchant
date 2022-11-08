8615c8acd804ea95980de79983896303673a963c
Sharlikran <danielhmpdx16@gmail.com>
2022-11-08 07:44:58 -0800

    Version: 3.7.23

--- MasterMerchant_Namespace_Init.lua
+++ MasterMerchant_Namespace_Init.lua
@@ -26,7 +26,7 @@ MMScrollList.SORT_KEYS = {
 
 MasterMerchant = { }
 MasterMerchant.name = 'MasterMerchant'
-MasterMerchant.version = '3.7.22'
+MasterMerchant.version = '3.7.23'
 
 -------------------------------------------------
 ----- early helper                          -----
